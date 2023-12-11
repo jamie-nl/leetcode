ddc829e31bbd205567d27eeded0854a7d88ee77f
Jamie Long <mrjamienlong@hotmail.com>
2023-12-11 16:41:47 +0000

    1920

---
+++ 1900/1920.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = []
+
+        for i in nums:
+            ans.append(nums[i])
+
+        return ans
